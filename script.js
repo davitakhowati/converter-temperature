8f57fce848ce92e04d477b6d391ae4c466fe53a2
davitakhowati <82552526+davitakhowati@users.noreply.github.com>
2021-04-24 14:16:24 +0700

    selesai design

--- script.js
+++ script.js
@@ -8,38 +8,38 @@ function clearField() {
 }
 function fahrenheitConverter() {
     var fromfah = document.getElementById("inputFahrenheit").value;
-    document.getElementById("outputFahrenheit").innerHTML = fromfah + " °F";
+    document.getElementById("outputFahrenheit").innerHTML = fromfah + "°F";
 
     var countfahtocel = (fromfah-32)/1.8;
     var fahtocel = countfahtocel.toFixed(2);
-    document.getElementById("outputCelcius").innerHTML = parseFloat(fahtocel) + " °C";
+    document.getElementById("outputCelcius").innerHTML = parseFloat(fahtocel) + "°C";
 
     var countfahtokel = ((fromfah-32)/1.8) + 273.15;
     var fahtokel = countfahtokel.toFixed(2);
     document.getElementById("outputKelvin").innerHTML = parseFloat(fahtokel) + " K";
 
     if ( fromfah == "") {
-        document.getElementById('outputFahrenheit').innerHTML = '';
-        document.getElementById('outputCelcius').innerHTML = '';
-        document.getElementById('outputKelvin').innerHTML = '';
+        document.getElementById('outputFahrenheit').innerHTML = '0°F';
+        document.getElementById('outputCelcius').innerHTML = '0°C';
+        document.getElementById('outputKelvin').innerHTML = '0 K';
     }
 }
 function celciusConverter() {
     var fromcel = document.getElementById("inputCelcius").value;
-    document.getElementById("outputCelcius").innerHTML = fromcel + " °C";
+    document.getElementById("outputCelcius").innerHTML = fromcel + "°C";
 
     var countceltofah = (fromcel*1.8)+32;
     var celtofah = countceltofah.toFixed(2);
-    document.getElementById("outputFahrenheit").innerHTML = parseFloat(celtofah) + " °F";
+    document.getElementById("outputFahrenheit").innerHTML = parseFloat(celtofah) + "°F";
 
     var countceltokel = parseFloat(fromcel)+273.15;
     var celtokel = countceltokel.toFixed(2);
     document.getElementById("outputKelvin").innerHTML = celtokel + " K";
 
     if ( fromcel == "") {
-        document.getElementById('outputFahrenheit').innerHTML = '';
-        document.getElementById('outputCelcius').innerHTML = '';
-        document.getElementById('outputKelvin').innerHTML = '';
+        document.getElementById('outputFahrenheit').innerHTML = '0°F';
+        document.getElementById('outputCelcius').innerHTML = '0°C';
+        document.getElementById('outputKelvin').innerHTML = '0 K';
     }
 }
 function kelvinConverter() {
@@ -48,15 +48,15 @@ function kelvinConverter() {
 
     var countkeltofah = ((fromkel-273.15)*1.8)+32;
     var keltofah = countkeltofah.toFixed(2);
-    document.getElementById("outputFahrenheit").innerHTML = parseFloat(keltofah) + " °F";
+    document.getElementById("outputFahrenheit").innerHTML = parseFloat(keltofah) + "°F";
 
     var countkeltocel = fromkel - 273.15;
     var keltocel = countkeltocel.toFixed(2);
-    document.getElementById("outputCelcius").innerHTML = parseFloat(keltocel) + " °C";
+    document.getElementById("outputCelcius").innerHTML = parseFloat(keltocel) + "°C";
 
     if ( fromkel == "") {
-        document.getElementById('outputFahrenheit').innerHTML = '';
-        document.getElementById('outputCelcius').innerHTML = '';
-        document.getElementById('outputKelvin').innerHTML = '';
+        document.getElementById('outputFahrenheit').innerHTML = '0°F';
+        document.getElementById('outputCelcius').innerHTML = '0°C';
+        document.getElementById('outputKelvin').innerHTML = '0 K';
     }
 }
